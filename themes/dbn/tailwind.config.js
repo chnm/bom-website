984f34d4336b4169d760031b4f797d32f4c20c1b
Jason Heppler <jason.heppler@gmail.com>
2022-12-14 08:59:55 -0600

    Settings for dbn color palette and fonts

--- themes/dbn/tailwind.config.js
+++ themes/dbn/tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["content/**/*.md", "layouts/**/*.html"],
+  content: ["content/**/*.md", "layouts/**/*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      serif: "Libre Baskerville, Playfair Display, serif",
+      sans: "Work Sans, sans-serif",
+    },
+    container: {
+      center: true,
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1496px",
+      },
+    },
+    extend: {
+      colors: {
+        dbn: {
+          green: "#3E3E32",
+          yellow: "#B1A569",
+          yellowdark: "#7D744A",
+          blue: "#96ADC8",
+          red: "#EF3054",
+          orange: "#c75000ff",
+          purple: "#7F5A83",
+        },
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require('tw-elements/dist/plugin'),
+    require('@tailwindcss/typography'),
+  ],
+};
